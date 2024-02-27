0cfabb033d5ceb0baa996e3ed0abf2b9a7079e5d
Aangtonio <aang@ciencias.unam.mx>
2024-02-27 02:43:56 -0600

    Usar estos comiits, son mucho mejores que git bash

--- si.R
+++ si.R
@@ -7,20 +7,22 @@ library(dplyr)
 data1 <- read.csv("AirMOSS_HarvardForest_SoilMoisture_2012-2013.csv" , header = TRUE)
 data2 <- read.csv('HarvardForest_dbh_ba_2012-10.csv',header = TRUE)
 
+View(data1)
 
 attach(data1)
 
 
 
 temp_suelo <-na_if(soil_temp  , -9999.0)
-summary(temp_suelo)
+conduc_soil <- na_if(conductivity_0.5.8, -9999.0)
 
+#Estaditico de la temperatura del suelo
+summary(temp_suelo)
 var(temp_suelo,na.rm = T)
 
 
-
+#estaditico de la conductividad
+summary(conduc_soil)
 # Suponiendo que ya tienes una variable llamada prueba
 # Recodificar -999.0 como NA en la variable prueba
-prueba <- prueba %>% 
-  recode("-999.0" = NA_real_, .default = .)
 
